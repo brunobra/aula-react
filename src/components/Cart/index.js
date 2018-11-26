2963ed20b23ed554ed25cb564579fbe4b9923039
Bruno Junqueira <brunootaviojunqueira@gmail.com>
2018-11-26 17:55:18 -0200

    Adding proptypes to Cart

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,18 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './index.css';
 
 import CartHeader from '../CartHeader';
 import Product from '../Product';
 
-const Cart = () => (
+const Cart = ({products}) => (
   <div className="Cart">
     <CartHeader
       title="Carrinho"
     />
-    <Product />
-    <Product />
-    <Product />
+    {products.map((product) => (
+      <Product
+        key={product.id}
+        name={product.name}
+        price={product.price}
+      />
+    ))}
   </div>
 );
+
+Cart.propTypes = {
+  products: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.oneOf([PropTypes.string, PropTypes.number]),
+      name: PropTypes.string,
+      price: PropTypes.number
+    })
+  )
+};
+
+Cart.defaultProps = {
+  products: []
+};
 
 export default Cart;
