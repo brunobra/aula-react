04d0d023a4241adf6fe3e4a122b353ff71b1618c
Bruno Junqueira <brunootaviojunqueira@gmail.com>
2018-11-26 13:46:21 -0200

    Adding product component

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import './index.css';
 
 import CartHeader from '../CartHeader';
+import Product from '../Product';
 
 class Cart extends Component {
   render() {
     return (
       <div className="Cart">
         <CartHeader />
+        <Product />
+        <Product />
+        <Product />
       </div>
     );
   }
---
+++ src/components/Product/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class Product extends Component {
+  render() {
+    return (
+      <div className="Product">
+        Produto
+      </div>
+    );
+  }
+}
+
+export default Product;
