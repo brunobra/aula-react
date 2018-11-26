615a6d9e2b9b6d3ca55c1abe00ccffdc1b593650
Bruno Junqueira <brunootaviojunqueira@gmail.com>
2018-11-26 13:19:12 -0200

    Adding CartHeader

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './index.css';
 
+import CartHeader from '../CartHeader';
+
 class Cart extends Component {
   render() {
     return (
       <div className="Cart">
-        <p>Cart</p>
+        <CartHeader />
       </div>
     );
   }
---
+++ src/components/CartHeader/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class CartHeader extends Component {
+  render() {
+    return (
+      <div className="CartHeader">
+        <p>CartHeader</p>
+      </div>
+    );
+  }
+}
+
+export default CartHeader;
