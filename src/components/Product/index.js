b1a7b32eff47ee1a0f821ee2cf2ee9c0b66c2f61
Bruno Junqueira <brunootaviojunqueira@gmail.com>
2018-11-26 17:51:19 -0200

    Adding props to Product

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './Product.css';
 import './ProductName.css';
 import './ProductPrice.css';
 
-const Product = () => (
+const Product = ({name, price}) => (
   <div className="Product">
      <span className="ProductName">
-      Produto
+      {name}
     </span>
     <span className="ProductPrice">
-      R$ 20,00
+      {`R$ ${price}`}
     </span>
   </div>
 );
+
+Product.propTypes = {
+  name: PropTypes.string,
+  price: PropTypes.number
+};
+
+Product.defaultProps = {
+  name: 'Produto',
+  price: 0
+};
 
 export default Product;
